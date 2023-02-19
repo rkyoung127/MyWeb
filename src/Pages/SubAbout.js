624c32baabf6e69e010d8b5adf4f77b6c4695d31
최가영 <gyty7@naver.com>
2023-02-19 15:18:42 +0900

    map, li 문제 해결

--- src/Pages/SubAbout.js
+++ src/Pages/SubAbout.js
@@ -1,5 +1,5 @@
 const SubAbout = () => {
-  const info = ["천천히, 꾸준하게 걸어가는 것을 지향합니다.", "항상 성장하려고 노력하며 발전하는 것을 지향합니다.", "끊임없이 질문하고 호기심을 갖고 고민합니다.", "사람들에게 도움을 주는 것을 좋아합니다.", "효율적으로 행동하는 것을 좋아합니다."]
+  const info = ["천천히, 꾸준하게 걸어가는 것을 지향합니다.", "항상 성장하려고 노력하며 발전하는 것을 지향합니다.", "끊임없이 질문하고 호기심을 갖고 고민합니다.", "사람들에게 도움을 주는 것을 좋아합니다.", "효율적으로 행동하는 것을 좋아합니다."];
   // const education = [
   //   {
   //     school: "고려대학교 세종캠퍼스",
@@ -8,21 +8,13 @@ const SubAbout = () => {
   //     date: "2018.03~2023.02"
   //   }]
 
+  const infoList = info.map((item) => <li>{item}</li>);
 
   return (
     <div className="card">
       <div className="card-body">
-        <ul>
-          {
-            info.map((item) => {
-              <li>{item}</li>
-              console.log("item", item)
-            }
-            )
-          }
-        </ul>
+        <ul>{infoList}</ul>
       </div>
-
     </div>
 
   )
