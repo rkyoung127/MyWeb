30dce2872c134ca8aba1d564af62fd1f1c610e71
최가영 <gyty7@naver.com>
2023-03-08 16:08:46 +0900

    public 폴더에 이미지 추가 및 불러오기

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,7 +1,29 @@
+import "../Styles/Skill.css"
+
 const Skills = () => {
   return (
-    <div>
-      <p>여기는 기술스택 들어올 페이지</p>
+    <div id="skillspage">
+      <div className="thirdpage">
+        <div className="text">
+          <p>Skills</p>
+          <p>프로젝트 사용 경험이 있는 기술입니다.</p>
+        </div>
+        <div className="stackimg">
+          <div>
+            <img src={process.env.PUBLIC_URL + "/react.png"} alt="react" />
+            {/* <p>React</p> */}
+            <img src={process.env.PUBLIC_URL + "/bootstrap.png"} alt="bootstrap" />
+            <img src={process.env.PUBLIC_URL + "/javascript.png"} alt="javascript" />
+          </div>
+          <div>
+            <img src={process.env.PUBLIC_URL + "/Node.js_logo.svg.png"} alt="nodejs" />
+            <img src={process.env.PUBLIC_URL + "/mongodb.png"} alt="mongodb" />
+          </div>
+          <div>
+            <img src={process.env.PUBLIC_URL + "/gitlab.png"} alt="gitlab" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
